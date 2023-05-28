d70b13a07911bae6dc08984d4222e4f0351a7edf
zDein <antonio_rodrigues@discente.ufg.br>
2023-05-28 20:25:49 -0300

    Adicionando as funções do programa principal

--- ContaBancaria/ContaBancaria/Program.cs
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -6,9 +6,36 @@ namespace Banco
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
-            Console.Write("AOSDKASKD");
-            Console.WriteLine("OUTRA WRITELINE");
+            Console.Write("Entre com o número da conta: ");
+            int numero_conta = Convert.ToInt16((Console.ReadLine()));
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)?");
+            char opcao = Convert.ToChar((Console.ReadLine()));
+
+            if(opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                double deposito = Convert.ToDouble((Console.ReadLine()));
+            }
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(p);
+
+            Console.Write("Entre com um valor para depósito: ");
+            deposito = Convert.ToDouble((Console.ReadLine()));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(p);
+
+            Console.Write("Entre com um valor para saque: ");
+            deposito = Convert.ToDouble((Console.ReadLine()));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(p);
+
         }
     }
 }
