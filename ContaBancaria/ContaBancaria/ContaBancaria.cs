e3a5d2329c5ff63ad77d5f4239693b2aae3617ad
zDein <antonio_rodrigues@discente.ufg.br>
2023-05-28 20:53:57 -0300

    Atualizando o programa principal adicionando as operações. Definindo e implementando as variáveis e operações na classe ContaBancária

--- ContaBancaria/ContaBancaria/ContaBancaria.cs
+++ ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ContaBancaria
+using System.Globalization;
+namespace Banco
 {
-    internal class ContaBancaria
+    class ContaBancaria
     {
+        private double _saldo;
+        public string Nome;
+        public int NumeroConta;
+
+        public ContaBancaria()
+        {
+            _saldo = 0;
+        }
+
+        public ContaBancaria(int numeroConta, string nome) : this()
+        {
+            if (nome != null && nome.Length > 2)
+            {
+                this.Nome = nome;
+            }
+            this.NumeroConta = numeroConta;
+        }
+
+        public void deposito(double valor)
+        {
+            _saldo += valor;
+        }
+        
+        public void saque(double valor) 
+        {
+            _saldo -= valor + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + Nome
+                + ", Saldo: "
+                + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
--- ContaBancaria/ContaBancaria/Program.cs
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Banco
 {
@@ -6,33 +7,43 @@ namespace Banco
     {
         static void Main()
         {
+            ContaBancaria p;
+
+            double deposito;
+
             Console.Write("Entre com o número da conta: ");
-            int numero_conta = Convert.ToInt16((Console.ReadLine()));
+            int numero_conta = Convert.ToInt16(Console.ReadLine());
 
             Console.Write("Entre com o titular da conta: ");
             string titular = Console.ReadLine();
 
-            Console.Write("Haverá depósito inicial (s/n)?");
+            p = new ContaBancaria(numero_conta, titular);
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
             char opcao = Convert.ToChar((Console.ReadLine()));
 
-            if(opcao == 's' || opcao == 'S')
+            if (opcao == 's' || opcao == 'S')
             {
                 Console.Write("Entre com o valor de depósito inicial: ");
-                double deposito = Convert.ToDouble((Console.ReadLine()));
+                deposito = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                p.deposito(deposito);
             }
 
+            Console.WriteLine();
             Console.WriteLine("Dados da conta: ");
             Console.WriteLine(p);
 
+            Console.WriteLine();
             Console.Write("Entre com um valor para depósito: ");
-            deposito = Convert.ToDouble((Console.ReadLine()));
-
+            deposito = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.deposito(deposito);
             Console.WriteLine("Dados da conta atualizados:");
             Console.WriteLine(p);
 
+            Console.WriteLine();
             Console.Write("Entre com um valor para saque: ");
-            deposito = Convert.ToDouble((Console.ReadLine()));
-
+            double saque = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.saque(saque);
             Console.WriteLine("Dados da conta atualizados:");
             Console.WriteLine(p);
 
